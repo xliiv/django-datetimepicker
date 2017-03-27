073bbf570dc046ab599079bae436c7dcde58f181
Pablo Vergés <pablo@soofa.ch>
2017-03-27 21:21:43 +0200

    simplifies Media assets

--- datetimepicker/widgets.py
+++ datetimepicker/widgets.py
@@ -12,43 +12,6 @@ from django.utils.html import conditional_escape
 
 
 class DateTimePicker(DateTimeInput):
-    class Media:
-        class JsFiles(object):
-            def __iter__(self):
-                yield 'https://code.jquery.com/jquery-2.2.4.min.js'
-                yield 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'
-                yield 'bootstrap3_datetime/js/moment.min.js'
-                yield 'bootstrap3_datetime/js/bootstrap-datetimepicker.min.js'
-                lang = translation.get_language()
-                if lang:
-                    lang = lang.lower()
-                    #There is language name that length>2 *or* contains uppercase.
-                    lang_map = {
-                        'ar-ma': 'ar-ma',
-                        'en-au': 'en-au',
-                        'en-ca': 'en-ca',
-                        'en-gb': 'en-gb',
-                        'en-us': 'en-us',
-                        'fa-ir': 'fa-ir',
-                        'fr-ca': 'fr-ca',
-                        'ms-my': 'ms-my',
-                        'pt-br': 'bt-BR',
-                        'rs-latin': 'rs-latin',
-                        'tzm-la': 'tzm-la',
-                        'tzm': 'tzm',
-                        'zh-cn': 'zh-CN',
-                        'zh-tw': 'zh-TW',
-                        'zh-hk': 'zh-TW',
-                    }
-                    if len(lang) > 2:
-                        lang = lang_map.get(lang, 'en-us')
-                    if lang not in ('en', 'en-us'):
-                        yield 'bootstrap3_datetime/js/locales/bootstrap-datetimepicker.%s.js' % (lang)
-
-        js = JsFiles()
-        css = {'all': ('bootstrap3_datetime/css/bootstrap-datetimepicker.min.css',
-                       'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
-                       'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css')}
 
     # http://momentjs.com/docs/#/parsing/string-format/
     # http://docs.python.org/2/library/datetime.html#strftime-strptime-behavior
@@ -183,3 +146,8 @@ class DateTimePicker(DateTimeInput):
             }
         )
         return html + js
+
+    class Media:
+        js = ('datetimepicker/vendor/js/jquery.min.js',
+              'datetimepicker/js/jquery.datetimepicker.full.js')
+        css = {'all': ('datetimepicker/css/jquery.datetimepicker.min.css',)}

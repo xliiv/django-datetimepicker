7f964e3ed0c79a558e66730efa7fa394f8376a65
Pablo Escodebar <escodebar@gmail.com>
2017-08-09 14:19:44 +0200

    Improve sample project
    ... displaying the raw as well as the clean data.

--- sample/sample/views.py
+++ sample/sample/views.py
@@ -2,7 +2,13 @@ from django.views.generic.edit import FormView
 
 from .forms import SampleForm
 
+
 class SampleView(FormView):
 
     form_class = SampleForm
     template_name = 'sample.html'
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update({'data': self.request.GET})
+        return kwargs

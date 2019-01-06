1964c9601640ef30007d7bd0ea008d8a04228336
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2019-01-06 14:10:44 +0100

    Add missing quotes

--- datetimepicker/templates/datetimepicker/loader.js
+++ datetimepicker/templates/datetimepicker/loader.js
@@ -1,6 +1,6 @@
 (function () {
 	$(document).ready(function() {
-        $.datetimepicker.setLocale({{ lang }});
+        $.datetimepicker.setLocale('{{ lang }}');
 		$("#{{ input_id }}").datetimepicker({{ options|safe }});
 	});
 })(jQuery);
